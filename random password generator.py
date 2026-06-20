4f925907525406db1f8e9e8c61790e388eb2438f
Da-Blitzreig <Stevensunzhou@gmail.com>
2026-06-20 12:02:57 -0400

    new change to calc

---
+++ random password generator.py
@@ -0,0 +1,17 @@
+import random
+
+print("Hello! Welcome to the number guessing game!")
+
+L = 10
+ss = random.randint(1, 10)
+
+while True:
+    guess = int(input("Guess the number from 1 to 10: "))
+
+    if guess < ss:
+        print("Too low!")
+    elif guess > ss:
+        print("Too high!")
+    else:
+        print("Correct! You win!")
+        break
