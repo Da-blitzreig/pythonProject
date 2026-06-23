6b12bc5fd860cf402056b30781f0d5ec8a14bd04
Da-Blitzreig <Stevensunzhou@gmail.com>
2026-06-23 19:36:45 -0400

    new change to calc

---
+++ Day_1/Day_1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+City = input("What's the name of the city you grew up in?")
+Pet = input("What's your pet's name?")
+print("The name of your band could be " + City + " " + Pet)
