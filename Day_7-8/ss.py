2d3cdea0a5a36a0b1ac84528c2ebba2444cf670e
Da-Blitzreig <Stevensunzhou@gmail.com>
2026-06-26 12:13:36 -0400

    ....

---
+++ Day_7-8/ss.py
@@ -0,0 +1,6 @@
+def sum_square(x, y):
+    return x**2 + y**2
+
+print(sum_square(6, 8))      
+print(sum_square(12, 68))
+print(sum_square(123, 389))
---
+++ Day_7-8/ss2.py
@@ -0,0 +1,6 @@
+def greet(*, name, age):
+    print(f"Hello {name}, you are {age} years old")
+
+
+greet(name="Tom", age=12)
+greet(name="Charlie", age=16)
