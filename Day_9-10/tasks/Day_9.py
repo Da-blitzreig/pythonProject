cdf8a94eb7e10104d0c0b1b310749546dcba7b6d
Da-Blitzreig <Stevensunzhou@gmail.com>
2026-06-28 08:54:41 -0400

    ....

---
+++ Day_9-10/tasks/Day_9.py
@@ -0,0 +1,22 @@
+
+student = {
+    "name": "Alice",
+    "age": 12,
+    "grade": "7",
+    "address_city": "Toronto"
+}
+
+
+print(student["name"])
+print(student["age"])
+print(student["grade"])
+print(student["address_city"])
+
+
+student["age"] = 13
+student["grade"] = "8"
+
+
+print(student["name"])
+print(student["age"])
+print(student["grade"])
