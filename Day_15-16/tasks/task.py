f0295805dd908162b61bcccddfce8826aea3b94c
David Sun <dsauvne@gmail.com>
2026-07-03 16:53:32 -0400

    Hi

--- Day_15-16/tasks/task.py
+++ Day_15-16/tasks/task.py
@@ -1,12 +1,21 @@
-def print_list(list_of_nums: list):
+def find_maximum(list_of_nums: list):
     index = 0
-    highest = list_of_nums[0]   # Start with the first number
+    highest = list_of_nums[0]
     while index < len(list_of_nums):
         if list_of_nums[index] > highest:
             highest = list_of_nums[index]
         index += 1
     print(highest)
+def find_minimum(list_of_nums: list):
+    index = 0
+    lowest = list_of_nums[0]
+    while index < len(list_of_nums):
+        if list_of_nums[index] < lowest:
+            lowest = list_of_nums[index]
+        index += 1
+    print(lowest)
 
 
 one = [56, 76, 36, 87, 4, 62, 36, 99, 85]
-print_list(one)
+find_maximum(one)
+find_minimum(one)
