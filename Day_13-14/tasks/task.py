c0fe6b4c5c93e96639d156f7d99b69dfbbd337bd
David Sun <dsauvne@gmail.com>
2026-07-02 20:31:49 -0400

    Hi

---
+++ Day_13-14/tasks/task.py
@@ -0,0 +1,11 @@
+def print_list(list_of_nums: list):
+    index = 0
+    while index < len(list_of_nums):
+        print(list_of_nums[index])
+        index += 1
+
+one = [56, 76, 36, 87, 4, 62, 36, 99, 85]
+two = [44, 97, 29, 89, 74, 2, 64, 93, 24, 76, 11]
+three = [76, 7, 9, 51, 70, 20, 72, 30, 44, 86, 12]
+
+print_list(one)
