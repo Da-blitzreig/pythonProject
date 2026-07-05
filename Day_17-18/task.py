d239c803dae17f2ce179729fc5cf2d7c6d78b53a
David Sun <dsauvne@gmail.com>
2026-07-05 16:38:22 -0400

    Hi

---
+++ Day_17-18/task.py
@@ -0,0 +1,12 @@
+def find_last_in_list(list_of_num: list):
+    last = list_of_num[-1]
+    print(last)
+def find_third_in_list(list_of_num: list):
+    last = list_of_num[2]
+    print(last)
+
+
+one = [11, 72, 15, 14,19, 1671]
+
+find_last_in_list(one)
+find_third_in_list(one)
