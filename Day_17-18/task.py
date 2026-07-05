2a4f8aa57ea1a0b71d46d9332c4c1c25627d2dc9
David Sun <dsauvne@gmail.com>
2026-07-05 17:09:37 -0400

    Hi

--- Day_17-18/task.py
+++ Day_17-18/task.py
@@ -4,9 +4,13 @@ def find_last_in_list(list_of_num: list):
 def find_third_in_list(list_of_num: list):
     last = list_of_num[2]
     print(last)
-
-
-one = [11, 72, 15, 14,19, 1671]
+def median_in_list(list_of_num:list):
+    median_of_list = len(list_of_num)
+    median1 = int(median_of_list / 2)
+    median = list_of_num[median1]
+    print(median)
+one = [11, 72, 15, 14, 19, 1671, 12]
 
 find_last_in_list(one)
 find_third_in_list(one)
+median_in_list(one)
