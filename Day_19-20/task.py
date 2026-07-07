606e860799f2b562963ab07a7ef1a7e6de073332
David Sun <dsauvne@gmail.com>
2026-07-07 19:42:51 -0400

    Hi

--- Day_19-20/task.py
+++ Day_19-20/task.py
@@ -3,9 +3,14 @@ def add_lists(first_list, second_list):
     for i in range(len(first_list)):
         index.append(first_list[i] + second_list[i])
     return index
-
+def multiply_lists(first_list, second_list):
+    index = []
+    for i in range(len(first_list)):
+        index.append(first_list[i] * second_list[i])
+    return index
 
 list1 = [1, 2, 3, 5, 7]
 list2 = [3, 5, 7, 1, 2]
 
 print(add_lists(list1, list2))
+print(multiply_lists(list1, list2))
