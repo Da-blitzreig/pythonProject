037ecb62b68b1cbad3b88ed3ba0b108a874be3a5
David Sun <dsauvne@gmail.com>
2026-07-09 14:40:37 -0400

    Hi

--- Day_21-22/task.py
+++ Day_21-22/task.py
@@ -3,7 +3,7 @@ def sort_integers(nums):
     for num in nums:
         if num % 2 == 0 and num % 3 == 0:
             result.append(num)
-    print(result)
+    return result
 
 numbers = [12, 7, 18, 25, 6, 30, 11, 24]
-sort_integers(numbers)
+print(sort_integers(numbers))
