f4573ef1a021a8fe66783893e67f522ce8454395
David Sun <dsauvne@gmail.com>
2026-07-16 14:45:24 -0400

    Hi

--- Day_29-30/task.py
+++ Day_29-30/task.py
@@ -1,12 +1,12 @@
 def runner_up(scores):
     scores = list(set(scores))
     scores.sort()
-    result = []
+    results = []
     index = len(scores) - 2
     while index >= 0:
-        result.append(scores[index])
+        results.append(scores[index])
         index -= 1
-    return result
+    return results
 
 score = [2, 3, 6, 7, 5]
 print(runner_up(score))
