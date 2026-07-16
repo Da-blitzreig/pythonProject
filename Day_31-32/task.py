8986f41785c45144862baea213346545faeae833
David Sun <dsauvne@gmail.com>
2026-07-16 15:58:02 -0400

    Hi

--- Day_31-32/task.py
+++ Day_31-32/task.py
@@ -1,10 +1,11 @@
-def median_in_list(list_of_num: list):
+def median_in_list(list_of_num):
+    list_of_num.sort()
     n = len(list_of_num)
     middle = n // 2
     if n % 2 == 1:
         print(list_of_num[middle])
     else:
-        print(list_of_num[middle - 1], list_of_num[middle])
+        print((list_of_num[middle - 1] + list_of_num[middle]) / 2)
 
 score = [2, 3, 6, 7, 5]
-print(median_in_list(score))
+median_in_list(score)
