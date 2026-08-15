9ecaf62d97061790f1302f009d9394c8291fbba8
David Sun <dsauvne@gmail.com>
2026-08-15 15:34:22 -0400

    hi

---
+++ Day_51-52/task.py
@@ -0,0 +1,26 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+player = input("Choose rock, paper, or scissors: ").lower()
+computer = random.choice(choices)
+
+print("Computer chose:", computer)
+
+if player == computer:
+    print("It's a tie!")
+
+elif player == "rock" and computer == "scissors":
+    print("You win!")
+
+elif player == "paper" and computer == "rock":
+    print("You win!")
+
+elif player == "scissors" and computer == "paper":
+    print("You win!")
+
+elif player in choices:
+    print("Computer wins!")
+
+else:
+    print("That's not a valid choice!")
